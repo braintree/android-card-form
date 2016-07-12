c5c31fc322af9a7ced0a26b97d09ad2e5d9f78f8
dhaval <dhavalsharma@gmail.com>
2016-07-12 12:34:43 +0530

    added name field to form

---
+++ CardForm/src/main/java/com/braintreepayments/cardform/view/CardNameEditText.java
@@ -0,0 +1,21 @@
+package com.braintreepayments.cardform.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by dhaval on 12/07/16.
+ */
+public class CardNameEditText extends FloatingLabelEditText {
+    public CardNameEditText(Context context) {
+        super(context);
+    }
+
+    public CardNameEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CardNameEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
