8d8275ebfbe530748bfe73063a0650b171a4d1d0
Emmanuel Lagarrigue <elagarrigue@gmail.com>
2018-10-18 13:14:32 -0300

    Clean up, set target 28

--- Sample/src/main/java/com/braintreepayments/sample/BaseCardFormActivity.java
+++ Sample/src/main/java/com/braintreepayments/sample/BaseCardFormActivity.java
@@ -1,7 +1,7 @@
 package com.braintreepayments.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.Toast;
