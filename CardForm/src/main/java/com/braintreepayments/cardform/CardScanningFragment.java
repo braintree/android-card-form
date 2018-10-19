f3e6c26fbe64ee8ac0a558c01c5e89c9af587721
quinnjn <quinn@neumiiller.com>
2018-10-19 15:47:16 -0700

    Bump gradle, build tools

--- CardForm/src/main/java/com/braintreepayments/cardform/CardScanningFragment.java
+++ CardForm/src/main/java/com/braintreepayments/cardform/CardScanningFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.VisibleForTesting;
 
 import com.braintreepayments.cardform.utils.ColorUtils;
 import com.braintreepayments.cardform.view.CardForm;
