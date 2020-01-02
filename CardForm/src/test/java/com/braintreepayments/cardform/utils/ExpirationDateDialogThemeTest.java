63f325411b16e9addc45e6d1c5784e614179c874
hollabaq86 <hollis65@me.com>
2020-01-02 15:32:53 -0600

    Update expiration date tests
    Co-authored-by: scannillo <sammyjaynecannillo@gmail.com>
    Co-authored-by: hollabaq86 <hollis65@me.com>

--- CardForm/src/test/java/com/braintreepayments/cardform/utils/ExpirationDateDialogThemeTest.java
+++ CardForm/src/test/java/com/braintreepayments/cardform/utils/ExpirationDateDialogThemeTest.java
@@ -16,20 +16,8 @@ import static junit.framework.Assert.assertEquals;
 public class ExpirationDateDialogThemeTest {
 
     @Test
-    public void detectTheme_detectsBlackBackgroundAndReturnsLightTheme() {
+    public void detectTheme_detectsBlackBackgroundAndReturnsDarkTheme() {
         Activity activity = setupActivity(R.color.bt_black);
-
-        ExpirationDateDialogTheme theme = ExpirationDateDialogTheme.detectTheme(activity);
-
-        assertEquals(ExpirationDateDialogTheme.LIGHT, theme);
-        assertEquals(getColor(R.color.bt_black_87), theme.getItemTextColor());
-        assertEquals(getColor(R.color.bt_white_87), theme.getItemInvertedTextColor());
-        assertEquals(getColor(R.color.bt_black_38), theme.getItemDisabledTextColor());
-    }
-
-    @Test
-    public void detectTheme_detectsWhiteBackgroundAndReturnsDarkTheme() {
-        Activity activity = setupActivity(R.color.bt_white);
 
         ExpirationDateDialogTheme theme = ExpirationDateDialogTheme.detectTheme(activity);
 
@@ -37,6 +25,18 @@ public class ExpirationDateDialogThemeTest {
         assertEquals(getColor(R.color.bt_white_87), theme.getItemTextColor());
         assertEquals(getColor(R.color.bt_black_87), theme.getItemInvertedTextColor());
         assertEquals(getColor(R.color.bt_white_38), theme.getItemDisabledTextColor());
+    }
+
+    @Test
+    public void detectTheme_detectsWhiteBackgroundAndReturnsLightTheme() {
+        Activity activity = setupActivity(R.color.bt_white);
+
+        ExpirationDateDialogTheme theme = ExpirationDateDialogTheme.detectTheme(activity);
+
+        assertEquals(ExpirationDateDialogTheme.LIGHT, theme);
+        assertEquals(getColor(R.color.bt_black_87), theme.getItemTextColor());
+        assertEquals(getColor(R.color.bt_white_87), theme.getItemInvertedTextColor());
+        assertEquals(getColor(R.color.bt_black_38), theme.getItemDisabledTextColor());
     }
 
     @Test
