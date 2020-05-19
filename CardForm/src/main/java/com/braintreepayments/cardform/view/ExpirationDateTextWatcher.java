86d6196eb3a856001bd26f63bfd69ba520e1f0c6
Steven Shropshire <steven.shropshire@getbraintree.com>
2020-05-19 09:11:52 -0500

    Add additional specs for formatting date string.

--- CardForm/src/main/java/com/braintreepayments/cardform/view/ExpirationDateTextWatcher.java
+++ CardForm/src/main/java/com/braintreepayments/cardform/view/ExpirationDateTextWatcher.java
@@ -3,7 +3,14 @@ package com.braintreepayments.cardform.view;
 import android.text.Editable;
 import android.text.TextWatcher;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 class ExpirationDateTextWatcher implements TextWatcher {
+
+    private static final Set<String> VALID_MONTHS = new HashSet<>(
+            Arrays.asList("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"));
 
     private static final int STATE_IDLE = 0;
     private static final int STATE_FORMATTING = 1;
@@ -46,7 +53,6 @@ class ExpirationDateTextWatcher implements TextWatcher {
         int originalTextLength = e.length();
         if (originalTextLength > 0) {
 
-            boolean didSeeNonZeroCharacter = false;
             for (int i = 0; i < originalTextLength; i++) {
                 char c = e.charAt(i);
 
@@ -56,6 +62,14 @@ class ExpirationDateTextWatcher implements TextWatcher {
                     buffer.append('0');
                 }
                 buffer.append(c);
+
+                boolean shouldInsertSlash = (buffer.length() == 2) && !VALID_MONTHS.contains(buffer.toString());
+                if (shouldInsertSlash) {
+                    buffer.insert(1, '/');
+                    if (buffer.charAt(0) == '1') {
+                        buffer.insert(0, '0');
+                    }
+                }
             }
 
             state = STATE_FORMATTING;
--- CardForm/src/test/java/com/braintreepayments/cardform/view/ExpirationDateTextWatcherTest.java
+++ CardForm/src/test/java/com/braintreepayments/cardform/view/ExpirationDateTextWatcherTest.java
@@ -68,7 +68,7 @@ public class ExpirationDateTextWatcherTest {
     }
 
     @Test
-    public void afterTextChanged_whenFirstTwoCharsEnteredRepresentValidMonth_doesNothing() {
+    public void afterTextChanged_whenFirstTwoCharactersAreValidMonth_doesNothing() {
 
         List<List<String>> assertionPairs = Arrays.asList(
                 Arrays.asList("01", "01"),
@@ -83,6 +83,83 @@ public class ExpirationDateTextWatcherTest {
                 Arrays.asList("10", "10"),
                 Arrays.asList("11", "11"),
                 Arrays.asList("12", "12")
+        );
+
+        for (List<String> pair : assertionPairs) {
+            String input = pair.get(0);
+            String expectedOutput = pair.get(1);
+
+            ExpirationDateTextWatcher sut = ExpirationDateTextWatcher.newInstance();
+            SpannableStringBuilder editable = new SpannableStringBuilder(input);
+
+            sut.afterTextChanged(editable);
+            assertEquals(editable.toString(), expectedOutput);
+        }
+    }
+
+    @Test
+    public void afterTextChanged_whenFirstTwoCharactersAreInvalidMonth_addsSlash() {
+
+        List<List<String>> assertionPairs = Arrays.asList(
+                Arrays.asList("13", "01/3"),
+                Arrays.asList("14", "01/4"),
+                Arrays.asList("15", "01/5"),
+                Arrays.asList("16", "01/6"),
+                Arrays.asList("17", "01/7"),
+                Arrays.asList("18", "01/8"),
+                Arrays.asList("19", "01/9")
+        );
+
+        for (List<String> pair : assertionPairs) {
+            String input = pair.get(0);
+            String expectedOutput = pair.get(1);
+
+            ExpirationDateTextWatcher sut = ExpirationDateTextWatcher.newInstance();
+            SpannableStringBuilder editable = new SpannableStringBuilder(input);
+
+            sut.afterTextChanged(editable);
+            assertEquals(editable.toString(), expectedOutput);
+        }
+    }
+
+    @Test
+    public void afterTextChanged_whenValidMonthEnteredWithSlash_doesNothing() {
+
+        List<List<String>> assertionPairs = Arrays.asList(
+                Arrays.asList("01/", "01/"),
+                Arrays.asList("02/", "02/"),
+                Arrays.asList("03/", "03/"),
+                Arrays.asList("04/", "04/"),
+                Arrays.asList("05/", "05/"),
+                Arrays.asList("06/", "06/"),
+                Arrays.asList("07/", "07/"),
+                Arrays.asList("08/", "08/"),
+                Arrays.asList("09/", "09/"),
+                Arrays.asList("10/", "10/"),
+                Arrays.asList("11/", "11/"),
+                Arrays.asList("12/", "12/")
+        );
+
+        for (List<String> pair : assertionPairs) {
+            String input = pair.get(0);
+            String expectedOutput = pair.get(1);
+
+            ExpirationDateTextWatcher sut = ExpirationDateTextWatcher.newInstance();
+            SpannableStringBuilder editable = new SpannableStringBuilder(input);
+
+            sut.afterTextChanged(editable);
+            assertEquals(editable.toString(), expectedOutput);
+        }
+    }
+
+    @Test
+    public void afterTextChanged_whenTypingDateString_doesNothing() {
+
+        List<List<String>> assertionPairs = Arrays.asList(
+                Arrays.asList("01/2", "01/2"),
+                Arrays.asList("01/20", "01/20"),
+                Arrays.asList("01/202", "01/202"),
+                Arrays.asList("01/2020", "01/2020")
         );
 
         for (List<String> pair : assertionPairs) {
