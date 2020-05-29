d05ac8a94dee75f65285e578526f5c8e953bb15f
Steven Shropshire <steven.shropshire@getbraintree.com>
2020-05-29 15:39:32 -0500

    Restore additional card brands in the supported cards array.

--- Sample/src/main/java/com/braintreepayments/sample/BaseCardFormActivity.java
+++ Sample/src/main/java/com/braintreepayments/sample/BaseCardFormActivity.java
@@ -16,11 +16,8 @@ public class BaseCardFormActivity extends AppCompatActivity implements OnCardFor
         CardEditText.OnCardTypeChangedListener {
 
     private static final CardType[] SUPPORTED_CARD_TYPES = { CardType.VISA, CardType.MASTERCARD, CardType.DISCOVER,
-            CardType.AMEX, CardType.DINERS_CLUB, CardType.JCB, CardType.MAESTRO, CardType.HIPERCARD };
-
-//    private static final CardType[] SUPPORTED_CARD_TYPES = { CardType.VISA, CardType.MASTERCARD, CardType.DISCOVER,
-//                CardType.AMEX, CardType.DINERS_CLUB, CardType.JCB, CardType.MAESTRO, CardType.UNIONPAY,
-//                CardType.HIPER, CardType.HIPERCARD };
+                CardType.AMEX, CardType.DINERS_CLUB, CardType.JCB, CardType.MAESTRO, CardType.UNIONPAY,
+                CardType.HIPER, CardType.HIPERCARD };
 
     private SupportedCardTypesView mSupportedCardTypesView;
 
