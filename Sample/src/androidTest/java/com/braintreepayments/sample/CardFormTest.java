e4a5109b9d8e314200b7e11c6e04666b90b97f55
Steven Shropshire <steven.shropshire@getbraintree.com>
2020-07-15 10:42:44 -0500

    Add integration test
    Signed-off-by: Sarah Koop <skoop@paypal.com>

---
+++ Sample/src/androidTest/java/com/braintreepayments/sample/CardFormTest.java
@@ -0,0 +1,29 @@
+package com.braintreepayments.sample;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.lukekorth.deviceautomator.AutomatorAssertion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.lukekorth.deviceautomator.AutomatorAssertion.visible;
+import static com.lukekorth.deviceautomator.DeviceAutomator.onDevice;
+import static com.lukekorth.deviceautomator.UiObjectMatcher.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class CardFormTest {
+
+    @Test(timeout = 60000)
+    public void cardForm_hasAllFields() {
+        ActivityScenario<LightThemeActivity> scenario = ActivityScenario.launch(LightThemeActivity.class);
+        onDevice(withText("Cardholder Name")).check(visible(true));
+        onDevice(withText("Card Number")).check(visible(true));
+        onDevice(withText("Expiration Date")).check(visible(true));
+        onDevice(withText("CVV")).check(visible(true));
+        onDevice(withText("Postal Code")).check(visible(true));
+        onDevice(withText("Country Code")).check(visible(true));
+        onDevice(withText("Mobile Number")).check(visible(true));
+    }
+}
