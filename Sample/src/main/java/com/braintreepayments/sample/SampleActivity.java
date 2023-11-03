2c5991694057372e57892433f7b93857cbd6b8d3
Sarah Koop <skoop@paypal.com>
2023-11-03 16:31:06 -0500

    Add bottom sheet dialog example

--- Sample/src/main/java/com/braintreepayments/sample/SampleActivity.java
+++ Sample/src/main/java/com/braintreepayments/sample/SampleActivity.java
@@ -1,5 +1,7 @@
 package com.braintreepayments.sample;
 
+import static com.braintreepayments.sample.BaseCardFormActivity.SUPPORTED_CARD_TYPES;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,6 +12,9 @@ import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+
+import com.braintreepayments.cardform.view.CardForm;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class SampleActivity extends AppCompatActivity {
     private static final int VIBRATE_PERMISSION_REQUEST = 10;
@@ -36,6 +41,8 @@ public class SampleActivity extends AppCompatActivity {
                 break;
             case R.id.vibrate_permission_enabled:
                 enableVibratePermission();
+            case R.id.bottom_sheet_form:
+                displayBottomSheet();
             default:
                 break;
         }
@@ -51,6 +58,27 @@ public class SampleActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.VIBRATE},
                 VIBRATE_PERMISSION_REQUEST);
+    }
+
+    private void displayBottomSheet() {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.card_form);
+        CardForm cardForm = bottomSheetDialog.findViewById(R.id.card_form);
+        cardForm.cardRequired(true)
+                .maskCardNumber(true)
+                .maskCvv(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .saveCardCheckBoxChecked(true)
+                .saveCardCheckBoxVisible(true)
+                .cardholderName(CardForm.FIELD_REQUIRED)
+                .mobileNumberExplanation("Make sure SMS is enabled for this mobile number")
+                .actionLabel(getString(R.string.purchase))
+                .setSupportedCardTypes(SUPPORTED_CARD_TYPES)
+                .setup(this);
+        bottomSheetDialog.show();
     }
 
     @Override
